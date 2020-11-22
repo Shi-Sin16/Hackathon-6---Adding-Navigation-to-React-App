0f54d003c9b02e96f042f3185be2231e6a4f6507
Shi-Sin16 <43728712+Shi-Sin16@users.noreply.github.com>
2020-11-22 16:52:25 +0530

    Change

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
-
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+import { Router, Switch } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        <a href="./About">About</a>
+        <a href="./Home">Home</a>
+      </div>
+    );
+  }
 }
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-function generateCodeFromObject(obj){
-    //return a code generated string
-   }
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./components/App";
+// function generateCodeFromObject(obj){
+//     //return a code generated string
+//    }
    
-module.exports=generateCodeFromObject;
+// module.exports=generateCodeFromObject;
 
 
 // ReactDOM.render(<App />, document.getElementById("root"));
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
+function generateCodeFromObject(obj) {
+  //return a code generated string
+}
+
+module.exports = generateCodeFromObject;
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
