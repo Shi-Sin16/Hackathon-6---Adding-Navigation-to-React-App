f79523789569a8af3cb354dd7676ede252a41954
Shi-Sin16 <43728712+Shi-Sin16@users.noreply.github.com>
2020-11-30 12:33:34 +0530

    wasedrtfgyhjk

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-//import { BrowserRouter } from "react-router-dom";
-// function generateCodeFromObject(obj) {
-//   //return a code generated string
-// }
 
-//module.exports = generateCodeFromObject;
+function generateCodeFromObject(obj) {
+  //return a code generated string
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+module.exports = generateCodeFromObject;
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//       <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
