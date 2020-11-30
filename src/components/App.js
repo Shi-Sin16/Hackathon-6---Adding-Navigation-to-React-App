2223ac092142d6700a3297a5e850ab50ac4ae20c
Shi-Sin16 <43728712+Shi-Sin16@users.noreply.github.com>
2020-11-30 12:24:52 +0530

    copied sir code..my is in vs

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,47 @@
 import React, { Component, useState } from "react";
+import { useLocation, BrowserRouter, Link } from "react-rou ter-dom";
+import { Route, Switch } from "react-router-dom";
 import "../styles/App.css";
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
-import About from "./About";
-import Home from "./Home";
-import LocatioDisplay from "./LocationDisplay";
 
 class App extends Component {
   render() {
     return (
       <div id="main">
         <BrowserRouter>
-        <LocatioDisplay />
-        <Link to="/about">About</Link>
-        <Link to="/">Home</Link>
-
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-          <Route path="*">
-            <div>No Match</div>
-          </Route>
-        </Switch>
+          <LocationDisplay />
+          <Switch>
+            <Route exact path="/about">
+              <>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <div>You are on the about Page</div>
+              </>
+            </Route>
+            <Route exact path="/">
+              <>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Home />
+              </>
+            </Route>
+            <Route path="*">
+              <div>No match</div>
+            </Route>
+          </Switch>
         </BrowserRouter>
       </div>
     );
   }
 }
 
+function Home() {
+  return <div>You are home</div>;
+}
+
+export function LocationDisplay() {
+  const { pathname } = useLocation();
+  return <div data-testid="location-display">{pathname}</div>;
+}
+
+// export { LocationDisplay };
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,17 +3,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 //import { BrowserRouter } from "react-router-dom";
-function generateCodeFromObject(obj) {
-  //return a code generated string
-}
+// function generateCodeFromObject(obj) {
+//   //return a code generated string
+// }
 
-module.exports = generateCodeFromObject;
+//module.exports = generateCodeFromObject;
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
+ReactDOM.render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
